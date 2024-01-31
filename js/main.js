0bd243f5a18eb4f2de5688b0abf93663c5f87975
andryueee <vasyukov2004@list.ru>
2024-01-31 16:45:34 +0700

    sixth

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 let app = new Vue({
     el: '#app',
+    methods: {
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+
+        addToCart() {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        }
+    },
+
     data: {
         product: "Socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
+        cart: 0,
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
-         ],
-         sizes:['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-         
+        ],
+
     }
- })
+})
+
  
