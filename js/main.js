44a51f1424ce41d575e970167c748e8298b4bd32
Andryueee <149645950+Andryueee@users.noreply.github.com>
2024-02-01 18:13:44 +0700

    tenth

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-Vue.component('product-details', {
-    props: {
-        details: {
-            type: Array,
-            required: true
-        }
-    },
-    template: `
-    <ul>
-      <li v-for="detail in details">{{ detail }}</li>
-    </ul>
-  `
-})
-
 Vue.component('product', {
     props: {
         premium: {
@@ -21,96 +7,121 @@ Vue.component('product', {
     },
     template: `
    <div class="product">
+    <div class="product-image">
+           <img :src="image" :alt="altText"/>
+       </div>
+
+       <div class="product-info">
+           <h1>{{ title }}</h1>
+           <p v-if="inStock">In stock</p>
+           <p v-else>Out of Stock</p>
+           <ul>
+               <li v-for="detail in details">{{ detail }}</li>
+           </ul>
+          <p>Shipping: {{ shipping }}</p>
+           <div
+                   class="color-box"
+                   v-for="(variant, index) in variants"
+                   :key="variant.variantId"
+                   :style="{ backgroundColor:variant.variantColor }"
+                   @mouseover="updateProduct(index)"
+           ></div>
+          
+
         
-      <div class="product-image">
-        <img :src="image" />
-      </div>
 
-      <div class="product-info">
-          <h1>{{ product }}</h1>
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-          <p>Shipping: {{ shipping }}</p>
-
-          <product-details :details="details"></product-details>
-          <p>Shipping: {{ shipping }}</p>
-            
-          <div class="color-box"
-               v-for="(variant, index) in variants" 
-               :key="variant.variantId"
-               :style="{ backgroundColor: variant.variantColor }"
-               @mouseover="updateProduct(index)"
-               >
-          </div> 
-
-          <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
-            >
-          Add to cart
-          </button>
-
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
-
-       </div>  
-    
-    </div>
-   `,
+           <button
+                   v-on:click="addToCart"
+                   :disabled="!inStock"
+                   :class="{ disabledButton: !inStock }"
+           >
+               Add to cart
+           </button>
+           <button 
+                   v-on:click="removeFromCart" 
+                   :disabled="!inStock"
+                   :class="{ disabledButton: !inStock }"
+           >
+                Remove from cart
+            </button>
+       
+       </div>
+   </div>
+ `,
     data() {
         return {
-            product: 'Socks',
+            product: "Socks",
             brand: 'Vue Mastery',
             selectedVariant: 0,
+            altText: "A pair of socks",
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
                     variantId: 2234,
                     variantColor: 'green',
-                    variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
                     variantQuantity: 10
                 },
                 {
                     variantId: 2235,
                     variantColor: 'blue',
-                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
                     variantQuantity: 0
                 }
             ],
-            cart: 0
         }
     },
     methods: {
-        addToCart: function() {
-            this.cart += 1
+
+        addToCart() {
+            this.$emit('add-to-cart',
+                this.variants[this.selectedVariant].variantId);
         },
-        updateProduct: function(index) {
-            this.selectedVariant = index
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+        removeFromCart: function() {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId)
         }
     },
     computed: {
         title() {
-            return this.brand + ' ' + this.product
+            return this.brand + ' ' + this.product;
         },
-        image(){
-            return this.variants[this.selectedVariant].variantImage
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
         shipping() {
             if (this.premium) {
-                return "Free"
+                return "Free";
+            } else {
+                return 2.99
             }
-            return 2.99
         }
     }
 })
-
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
-        premium: true
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        removeItem(id) {
+            var index = this.cart.lastIndexOf(id);
+            if (index !== -1) {
+                this.cart.splice(index, 1);
+            }
+        }
     }
+
+
 })
+
